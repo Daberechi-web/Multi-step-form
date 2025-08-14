cd9d877ce17c463cbfa7299f9fb3635e1a2b2bf2
daberechi-web <daberechi633@gmail.com>
2025-08-14 20:15:54 +0100

    fix casing issue in home.jsx import

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import HomePage from './Pages/home'
 import Contact from './Pages/Form'
 import Confirm from './Pages/Confirmation'
 import Done from './Pages/Sucess'
+import HomePage from './Pages/home'
 
 function App() {
        const [formData, setFormData] = useState({
@@ -22,7 +22,7 @@ function App() {
       
 
       <Routes>
-        <Route path='/' element= {<HomePage formData={formData} setFormData={setFormData} />} />
+        <Route path='/' element = { <HomePage formData={formData} setFormData={setFormData} />} />
         <Route path= '/Contact' element= {<Contact formData={formData} setFormData={setFormData} />} />
         <Route path='Confirm' element = {<Confirm formData={formData}  />} />
         <Route path='Done' element= {<Done />} />
